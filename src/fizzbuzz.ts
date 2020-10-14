be45868748d4694ce9c4f25becf97c858858016e
Katie Jones <katieljones29@gmail.com>
2020-10-14 15:25:07 +0100

    everything else returns number

--- src/fizzbuzz.ts
+++ src/fizzbuzz.ts
@@ -1,7 +1,17 @@
 export class FizzBuzz {
 
+  generate(number: number) {
+    let output: string[];
+    for (let i = 1; i <= number; i++) {
+        output.push(this.replaceNumber(i));
+    }
+    return output;
+  }
   replaceNumber(number: number): string {
+    if(number%3===0 && number%5===0) return "FizzBuzz";
     if(number%3===0) return "Fizz";
     if(number%5===0) return "Buzz";
+    else return `${number}`;
+
   }
 }
--- test/fizzbuzz_test.ts
+++ test/fizzbuzz_test.ts
@@ -5,11 +5,26 @@ describe('fizzbuzz', () => {
   it('should be divisible by 3 and replace number with Fizz', () => {
     let fizzbuzz = new FizzBuzz()
     expect(fizzbuzz.replaceNumber(3)).to.equal("Fizz")
+    expect(fizzbuzz.replaceNumber(6)).to.equal("Fizz")
+
   })
 
 
   it('should be divisible by 5 and replace number with Buzz', () => {
     let fizzbuzz = new FizzBuzz()
     expect(fizzbuzz.replaceNumber(5)).to.equal("Buzz")
+    expect(fizzbuzz.replaceNumber(10)).to.equal("Buzz")
+
+  })
+
+
+
+  it('should return a number if it is neither divisible by 3,5,15', () => {
+    let fizzbuzz = new FizzBuzz()
+    expect(fizzbuzz.replaceNumber(4)).to.equal('4')
+    expect(fizzbuzz.replaceNumber(15)).to.equal('15')
+
+
+
   })
 })
