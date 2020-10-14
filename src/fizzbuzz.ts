747b609af6037a841676de9df1197da6d64f3419
Katie Jones <katieljones29@gmail.com>
2020-10-14 14:48:53 +0100

    number divisble by 3 is replaced with word fizz

--- src/fizzbuzz.ts
+++ src/fizzbuzz.ts
@@ -1,1 +1,7 @@
-export
+export class FizzBuzz {
+
+
+  getWord(number: number): string {
+    if(number%3===0) return "Fizz";
+  }
+}
--- test/fizzbuzz_test.ts
+++ test/fizzbuzz_test.ts
@@ -1,1 +1,9 @@
-import
+import {FizzBuzz} from '../src/fizzbuzz'
+import {expect} from 'chai'
+
+describe('fizzbuzz', () => {
+  it('should be divisible by 3 and replace number with Fizz', () => {
+    let fizzbuzz = new FizzBuzz()
+    expect(fizzbuzz.getWord(3)).to.equal("Fizz")
+  })
+})
